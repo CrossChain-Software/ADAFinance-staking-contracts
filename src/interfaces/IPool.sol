5371fe8fa1ead1a25dd85ed417376af9d60e235a
Bradley Woolf <bradleymwoolf@gmail.com>
2021-11-10 11:17:18 -0800

    commenting out to go through one by one

--- src/interfaces/IPool.sol
+++ src/interfaces/IPool.sol
@@ -4,20 +4,35 @@ pragma solidity ^0.8.0;
 
 interface IPool {
 
+     struct Deposit {
+        // @dev token amount deposited
+        uint256 tokenAmount;
+        // @dev stake weight
+        uint256 timestamp;
+    }
+
+    function getDeposit(address _user, uint256 _depositId) external view returns (Deposit memory);
+    
     function getIncentivesSupply() external view returns (uint256);
     
-    function getIncentivesRemaining() external view returns (uint256);
+    // function getIncentivesRemaining() external view returns (uint256);
     
-    function getIncentiveLevels() external view returns (uint256);
+    // function getIncentiveLevels() external view returns (uint256);
     
-    function getDepositFee() external view returns (uint256);
+    // function getDepositFee() external view returns (uint256);
     
-    function getWithdrawalFee() external view returns (uint256);
+    // function getWithdrawalFee() external view returns (uint256);
     
-    function getMinAmount() external view returns (uint256);
+    // function getMinAmount() external view returns (uint256);
     
-    function getFeeDistribution() external view returns (uint256);
+    // function getFeeDistribution() external view returns (uint256);
     
-    function getTotalStaked() external view returns (uint256);
+    // function getTotalStaked() external view returns (uint256);
+
+    // function stake(uint256 _amount) external;
+
+    // function unstake(uint256 _amount) external;
+
+    // function calcRewards(uint256 _amount) external;
 
 }
