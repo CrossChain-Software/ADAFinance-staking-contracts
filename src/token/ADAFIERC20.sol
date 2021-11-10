14388420dbcd62611f976bbb7e6a2e82646ee857
Bradley Woolf <bradleymwoolf@gmail.com>
2021-11-10 09:28:16 -0800

    token done

--- src/token/ADAFIERC20.sol
+++ src/token/ADAFIERC20.sol
@@ -1,12 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../utils/ERC20.sol";
+import "../utils/Ownable.sol";
 
- contract ADAFI is ERC20 {
+ contract ADAFI is ERC20, Ownable {
 
-     constructor(uint256 _supply) ERC20("ADA", "ADAFI") {
-         _mint(msg.sender, _supply * (10 ** decimals()));
-     }
+    constructor(uint256 _supply) ERC20("ADA", "ADAFI") {
+        _mint(msg.sender, _supply * (10 ** decimals()));
+    }
+
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    function _transferOwnership(address _newOwner) public onlyOwner { 
+        transferOwnership(_newOwner);
+    }
 
  }
