338d4ba9b68713c849196a5d214a3aecca0efa52
Bradley Woolf <bradleymwoolf@gmail.com>
2021-11-11 11:23:58 -0800

    testing

--- src/PoolFactory.t.sol
+++ src/PoolFactory.t.sol
@@ -14,6 +14,15 @@ contract PoolFactoryTest is DSTest {
     function testFailsIfNotMult2() public {
         uint result = poolFactory.dappTest(4);
         uint testGas = result * 2;
+        emit log("ahh im test logginggggggg");
+        emit log_uint(testGas);
         assertEq(testGas, 8);
+    }
+
+    function testCreateStakingPool() public {
+        emit log("ahh im test logginggggggg");
+        // baseMod = 100
+        uint result = poolFactory.createStakingPool(1000, [1500,2000], 75, 75, 1000, [20, 30], 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7);
+        assertEq(result);
     }
 }
